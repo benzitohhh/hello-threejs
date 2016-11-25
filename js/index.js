ad9c96cb90720c83e23c7ad9e18de31dffaacab3
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2016-11-25 13:14:08 +0000

    and some animation

--- js/index.js
+++ js/index.js
@@ -7,7 +7,9 @@ document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
+var cube = new THREE.Mesh(geometry, material); // this is an instance of Object3D.
+                                               // It has a matrix which stores its position/rotation/scale.
+
 scene.add(cube);
 
 camera.position.z = 5;
@@ -16,6 +18,11 @@ function render() {
   requestAnimationFrame(render);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+
+  cube.position.x += 0.01;
+  if (cube.position.x > 3) {
+    cube.position.x = -2;
+  }
   renderer.render(scene, camera);
 }
 render();
